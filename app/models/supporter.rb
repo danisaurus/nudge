8e7900b0114bc14b394de9e12a9ec450ebfbe774
Raphael <boo.rho@gmail.com>
2014-11-07 15:22:02 -0600

    added Users, Supporters, and some basic Twilio integration

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,9 +1,10 @@
 class Supporter < ActiveRecord::Base
   belongs_to :user
-  phony_normalize :phone_number, :default_country_code => 'US'
-  validates :phone_number, :phony_plausible => true
+  phony_normalize :phone, :default_country_code => 'US' # takes care of the many different ways to input a phone number
+  validates :phone, :phony_plausible => true
 
   validate :must_have_a_way_to_contact
+  validates :first_name, presence: true
 
 
   def must_have_a_way_to_contact
@@ -12,7 +13,14 @@ class Supporter < ActiveRecord::Base
   	end
   end
 
-  def call
+  # Make sure those environment variables are set before running
+  def text(message)
+  	client = Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"])
+  	client.account.messages.create({
+  		from: ENV['TWILIO_NUM'],
+  		to: self.phone,
+  		body: message
+  		})
   end
 
 end
--- spec/models/supporter_spec.rb
+++ spec/models/supporter_spec.rb
@@ -4,18 +4,23 @@ RSpec.describe Supporter, :type => :model do
 
 	describe "validations" do
 		it "should not be a valid supporter if they don't have a way to be contacted" do
-			not_valid = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+			not_valid = Supporter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 			expect(not_valid.valid?).to be_falsey
 		end
 
 		it "should be a valid supporter if they have a way to be contacted" do
-			valid = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, gmail: Faker::Internet.email)
+			valid = Supporter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, gmail: Faker::Internet.email)
 			expect(valid.valid?).to be_truthy
 		end
 
 		it "should not accept invalid phone numbers" do
-			not_valid = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: "123")
+			not_valid = Supporter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: "123")
 			expect(not_valid.valid?).to be_falsey
+		end
+
+		it "should be okay with valid phone numbers" do
+			valid = Supporter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: "(773)-633-3299")
+			expect(valid.valid?).to be_truthy
 		end
 	end
 
