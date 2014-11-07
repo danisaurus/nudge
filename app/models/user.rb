2a10a4fd3a3afded120b35b92d0222bcf3167d49
Raphael <boo.rho@gmail.com>
2014-11-07 15:25:58 -0600

    had some scaffold stuff leftover

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   has_many :supporters
   phony_normalize :phone, :default_country_code => 'US'
 
-  validates :first_name, presence: true
-  validates :gmail, presence: true
 end
