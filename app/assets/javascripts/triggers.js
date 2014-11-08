45be7131fc43958f5c339e876fb081c2aa5cc974
Abigail Tuckner <abbietuckner@gmail.com>
2014-11-07 19:37:33 -0600

    quick fix for number of days

--- app/assets/javascripts/triggers.js
+++ app/assets/javascripts/triggers.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var days = $("#trigger_duration_in_hours")
+  days.val(1);
 
   $("body").on( "click", ".toggle-up", function() {
     days.val(Number(days.val()) + 1);
