6e9b9e219597be75dfe778a5ed4021d886f2ac9a
Raphael <boo.rho@gmail.com>
2014-11-08 13:02:54 -0600

    added confirm message for supporter delete and finished the rest of the crud stuff for users

--- app/assets/javascripts/supporters.js
+++ app/assets/javascripts/supporters.js
@@ -2,22 +2,27 @@ $(document).ready(function(){
 
 	$("#supporters-container").on("click", ".supporter-delete", function(event){
 		event.preventDefault();
-		var type = $(this).data('action');
-		var url = $(this).attr('href');
 		var supporter = $(this).closest('.supporter-item');
-		$.ajax({
-			url: url,
-			type: type,
-			success: function(response){
-				console.log(response);
-				$(supporter).remove();
+		console.log(supporter);
+		var supporterName = $(supporter).find(".supporter-name").text();
+		if (confirm("Are you sure you want to remove " + supporterName + " from your circle?")){
+			var type = $(this).data('action');
+			var url = $(this).attr('href');
+			$.ajax({
+				url: url,
+				type: type,
+				success: function(response){
+					console.log(response);
+					$('#notice').text(response.notice);
+					supporter.remove();
 
-			},
-			error: function(response) {
-				console.log("Error during supporter delete");
-				console.log(response);
-			}
-		});
+				},
+				error: function(response) {
+					console.log("Error during supporter delete");
+					console.log(response);
+				}
+			});
+		}
 	});
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,33 +4,48 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		# @user = curent_user
+		@user = User.find(params[:user_id]) # uncomment line above once we have a current_user method
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		# @user = curent_user
+		@user = User.find(params[:user_id]) # uncomment line above once we have a current_user method
 	end
 
 	def update
-		@user = User.find(params[:id])
+		# @user = curent_user
+		@user = User.find(params[:user_id]) # uncomment line above once we have a current_user method
 		respond_to do |format|
 		  if @user.update(user_params)
 		    format.html { redirect_to @user, notice: 'User was successfully updated.' }
-		    # format.json { render :show, status: :ok, location: @user }
 		  else
 		    format.html { render :edit }
-		    # format.json { render json: @user.errors, status: :unprocessable_entity }
 		  end
 		end
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(user_params)
+		respond_to do |format|
+		  if @user.save
+		    format.html { redirect_to @user, notice: 'User was successfully created.' }
+		  else
+		    format.html { render :new }
+		  end
+		end
 	end
 
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		respond_to do |format|
+			format.html { return redirect_to root_url, notice: "User was successfully destroyed."}
+		end
 	end
 	
 	private
