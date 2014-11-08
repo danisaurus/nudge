3603b46b07b06e3289d77498808cfbd7a1b531b8
Apprentice <mans.puri@gmail.com>
2014-11-08 17:35:28 -0600

    added user logic, added migration table to include access_token for user, moved history_getter logice to vendor directory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
+require 'history_getter'
+
 class User < ActiveRecord::Base
   has_many :supporters
   has_many :triggers
   has_many :trigger_histories
   phony_normalize :phone, :default_country_code => 'US'
+
+  def active?
+
+  end
+
+  def find_last_history_id
+    history_getter = HistoryGetter.new(self.gmail, self.token.last.fresh_token)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107235737) do
+ActiveRecord::Schema.define(version: 20141108225458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gmail_accounts", force: true do |t|
+    t.string  "last_history_number"
+    t.integer "user_id"
+  end
 
   create_table "supporters", force: true do |t|
     t.integer  "user_id"
