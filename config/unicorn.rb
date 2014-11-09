aab7b45c96b52fccb9b99350ff9388171551b29e
Dani Dirks <dani.dirks@build.com>
2014-11-09 17:59:52 -0600

    adding client config to unicorn rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,10 +13,15 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
+
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+
+  Sidekiq.configure_client do |config|
+     config.redis = {url: ENV["REDISCLOUD_URL"], namespace: 'sidekiq'}
+  end
+
 end
