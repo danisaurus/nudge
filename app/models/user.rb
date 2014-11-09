c918bf635a5ebc7d572b502ec3779b4e443ea24b
Apprentice <mans.puri@gmail.com>
2014-11-08 19:46:15 -0600

    added active methods to user model and history_getter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ActiveRecord::Base
   has_many :supporters
   has_many :triggers
   has_many :trigger_histories
+  has_many :tokens
   phony_normalize :phone, :default_country_code => 'US'
 
   def active?
-
+    self.last_history_number != self.find_last_history_id
   end
 
   def find_last_history_id
-    history_getter = HistoryGetter.new(self.gmail, self.token.last.fresh_token)
+    history_getter = HistoryGetter.new("87265a@gmail.com", "ya29.uAAnm3VCNcKgBkHF0L1a5PhpcxXQI9xX4wIxdqjDa1h5KcaIS165cb75YToDa3gDHatH-w0U-VKVwQ")
+    history_getter.get_last_history_id
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108225458) do
+ActiveRecord::Schema.define(version: 20141108233126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141108225458) do
     t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "trigger_histories", force: true do |t|
