673fa55fecffcb4771ebbb7cc8a533d29b92897b
Raphael <boo.rho@gmail.com>
2014-11-10 08:24:36 -0600

    refactoring

--- vendor/gmail_api_client.rb
+++ vendor/gmail_api_client.rb
@@ -1,44 +1,32 @@
 require 'google/api_client'
 class GmailAPI
+  attr_accessor :token, :client, :gmail
 
   def initialize(token)
     @token = token
     @client = Google::APIClient.new(
       application_name: 'Gmail Alerts',
       application_version: '1.0.0')
+    @client.authorization.access_token = @token.fresh_token
     @gmail = @client.discovered_api('gmail', 'v1')
-    @client.authorization.access_token = @token.fresh_token #"ya29.uQBXW-pBGTN-INjHZtpQI3pez3L2T4weQUxr2FaYWz5jPgyL7OS3E5Tadmn7BrTcbt5o8mCgzya06g"
   end
 
-  def get_message_list
-    result = @client.execute(
-      api_method: @gmail.users.messages.list,
-      parameters: {
-        maxResults: 10,
-        userId: "me"},
-      headers: {'Content-Type' => 'application/json'}
-      )
-    return result
-  end
-
-  def get_message(message_id)
-    message = @client.execute(
-      api_method: @gmail.users.messages.get,
-      parameters: {
-        userId: "me",
-        id: message_id},
-      headers: {'Content-Type' => 'application/json'}
-      )
-  end
 
   def get_start_history_id
-    message_id = JSON.parse(get_message_list.body)['messages'][0]['id']
-    message = get_message(message_id)
-    JSON.parse(message.body)['historyId'].to_i
+    first_message_id = messages.data.messages[0].id
+    message = get_message(first_message_id)
+    message.data.history_id
   end
 
+
+  def get_last_history_id
+    get_history.data.historyId
+  end
+
+  private
+
   def get_history
-    history = @client.execute(
+    @client.execute(
       api_method: @gmail.users.history.list,
       parameters: {
         userId: "me",
@@ -46,10 +34,25 @@ class GmailAPI
         headers: {'Content-Type' => 'application/json'}
       )
   end
-
-  def get_last_history_id
-    JSON.parse(get_history.body)['historyId']
+  
+  def get_message_list
+    @client.execute(
+      api_method: @gmail.users.messages.list,
+      parameters: {
+        maxResults: 10,
+        userId: "me"},
+      headers: {'Content-Type' => 'application/json'}
+      )
   end
 
+  def get_message(message_id)
+    @client.execute(
+      api_method: @gmail.users.messages.get,
+      parameters: {
+        userId: "me",
+        id: message_id},
+      headers: {'Content-Type' => 'application/json'}
+      )
+  end
 
 end
