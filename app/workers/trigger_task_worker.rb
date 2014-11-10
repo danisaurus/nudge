f68d80d4588086e549eea94eb21d62e569491053
Dani Dirks <dani.dirks@build.com>
2014-11-10 10:04:39 -0600

    bundled gems

--- app/workers/trigger_task_worker.rb
+++ app/workers/trigger_task_worker.rb
@@ -5,6 +5,7 @@ class TriggerTaskWorker
       trigger = Trigger.find(trigger_id)
       user = User.find(trigger.user_id)
       user.send(trigger.task.method, trigger)
+      logger.info "Is this working correctly with sidekiq?"
   end
 
 end
