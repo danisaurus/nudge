081e7a72b23aff9103901e626e72389450699cb6
Matt Meyers <m.j.mey3rs@gmail.com>
2014-11-11 10:32:20 -0600

    Fix bugs on get started page, added create trigger to user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   has_many :triggers
   has_many :trigger_histories
   has_many :tokens
-  # phony_normalize :phone, :default_country_code => 'US'
 
   has_secure_password
 
