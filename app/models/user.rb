651ace401fe9de352c2abe16b8c085ee885150af
Matt Meyers <m.j.mey3rs@gmail.com>
2014-11-11 08:48:24 -0600

    Merging with master

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :triggers
   has_many :trigger_histories
   has_many :tokens
-  phony_normalize :phone, :default_country_code => 'US'
+  # phony_normalize :phone, :default_country_code => 'US'
 
   has_secure_password
 
