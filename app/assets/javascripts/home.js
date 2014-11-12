931a62c1c32e8b49756f419101ee11a0184f1d04
Dani Dirks <dani.dirks@build.com>
2014-11-12 16:54:20 -0600

    getting that wheel of feels looking so good

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,30 +2,27 @@ $(document).ready(function(){
 	var moodVal = 0 
 	$('.ss-sun').on('click', function(){
 		moodVal = 1.0
-		console.log(moodVal)
+
 	});
 	$('.ss-sunset').on('click', function(){
 		moodVal = 0.66
-		console.log(moodVal)
+
 	});
 	$('.ss-cloudy').on('click', function(){
 		moodVal = 0.33
-		console.log(moodVal)
+
 	});
 
 	$('.ss-rainy').on('click', function(){
 		moodVal = -0.33
-		console.log(moodVal)
 
 	});
 	$('.ss-storm').on('click', function(){
 		moodVal = -0.66
-		console.log(moodVal)
 	});
 
 	$('.ss-tornado').on('click', function(){
 		moodVal = -1.0
-		console.log(moodVal)
 	});
 	
 	$('#mood-submit').on('click', function(e){
