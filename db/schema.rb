ff0f2c0e28acc8e90d00ec9e19f4e27a26cccf1c
Mansi Puri <mans.puri@gmail.com>
2014-11-12 09:02:59 -0600

    testing errors

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 20141111165428) do
 
   create_table "daily_reports", force: true do |t|
     t.integer  "user_id"
-    t.float    "average_tweet_sentiment"
-    t.float    "average_email_sentiment"
     t.date     "created_at"
     t.datetime "updated_at"
   end
@@ -88,11 +86,7 @@ ActiveRecord::Schema.define(version: 20141111165428) do
     t.integer  "user_id"
     t.text     "message_text"
     t.integer  "duration_in_hours"
-<<<<<<< HEAD
-    t.datetime "time_last_run",     default: '2014-11-11 18:24:46'
-=======
-    t.datetime "time_last_run",     default: '2014-11-12 00:24:08'
->>>>>>> master
+    t.datetime "time_last_run",     default: '2014-11-11 14:44:25'
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "active",            default: true
