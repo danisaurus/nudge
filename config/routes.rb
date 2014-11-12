34794c0ea62a95c028bfe37267fd3c3b22b1208b
Mansi Puri <mans.puri@gmail.com>
2014-11-12 10:47:16 -0600

    Fixed new user creation whenever you refresh on get started page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :sessions
   get '/logout' => 'sessions#destroy', as: 'logout'
-
+  get '/users/get_started' => 'users#get_started', as: 'get_started'
   get '/users/:user_id/toggle_twitter_triggers' => 'users#toggle_twitter_triggers', as: 'toggle_twitter_trig'
   get '/users/:user_id/toggle_google_triggers' => 'users#toggle_google_triggers', as: 'toggle_google_trig'
   get '/users/:trigger_id/toggle' => 'users#specific_toggle', as: 'trig_toggle'
