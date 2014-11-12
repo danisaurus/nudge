6a89754286a9f1292f6444eb9967add5202b3cb4
Raphael <boo.rho@gmail.com>
2014-11-12 14:49:50 -0600

    added empty check for daily reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
   end
 
   def happy_messages?(number_of_days, data_type)
-    return true if daily_reports.empty?
+    return true if (daily_reports.empty? || daily_reports.send(data_type).empty?)
     reports = get_last_reports(number_of_days)
     avg_sentiment = reports.map{|report| report.data_average(data_type)}.inject(:+) / reports.count.to_f
     return avg_sentiment > -0.6
