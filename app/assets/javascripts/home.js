379b834e6fb77afc3526b40434e26f6b3b955014
Dani Dirks <dani.dirks@build.com>
2014-11-13 00:07:39 -0600

    got that damn wheel o feels working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,21 +2,18 @@ $(document).ready(function(){
 	var moodVal = 0 
 	$('.ss-sun').on('click', function(){
 		moodVal = 1.0
-
 	});
 	$('.ss-sunset').on('click', function(){
 		moodVal = 0.66
-
 	});
 	$('.ss-cloudy').on('click', function(){
 		moodVal = 0.33
-
 	});
 
 	$('.ss-rainy').on('click', function(){
 		moodVal = -0.33
-
 	});
+
 	$('.ss-storm').on('click', function(){
 		moodVal = -0.66
 	});
@@ -28,10 +25,13 @@ $(document).ready(function(){
 	$('#mood-submit').on('click', function(e){
 		e.preventDefault();
 		var url = '/moods'
-		var moodData = {'quantitative': moodVal}
-		$.post(url, moodData, function(serverResponse, status, jqXHR) {
-			console.log('moodData')
-	    });
+		if (moodVal === 0){
+			$('#error').text('( please choose a mood )')
+		} else {
+			var moodData = {'quantitative': moodVal}
+			$.post(url, moodData, function(serverResponse, status, jqXHR) {
+			});
+		}
 	})
 
 });
